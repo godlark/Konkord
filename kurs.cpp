88963e1e953a3edf1e63e8e8984e429b79693b0b
godlark@gmail.com <godlark@gmail.com>
2010-12-21 06:48:12 +0100

    Zmiana wynikająca ze sposobu obliczania priorytetu w funkcji wybierającej słowa do powtórzenia

--- kurs.cpp
+++ kurs.cpp
@@ -194,7 +194,7 @@ vector<ushort> Kurs::getWordsToRepetition(ushort &howManyWords) const{
 		}
 		wtr.priority /= countConnections;
 		wtr.nr_word = i;
-		if(wtr.priority > -0.1) {
+		if(wtr.priority > 700) {
 			AhowManyWords++;
 		}
 		Q.push(wtr);
@@ -222,7 +222,7 @@ vector<ushort> Kurs::getWordsToRepetition(ushort &howManyWords) const{
 		}
 		wtr.priority /= countConnections;
 		wtr.nr_word = i;
-		if(wtr.priority > -0.1) {
+		if(wtr.priority > 700) {
 			AhowManyWords++;
 		}
 		Q.push(wtr);
