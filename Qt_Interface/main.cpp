970c55c39e237ff81c6982e2d3097e9e06cd6e95
godlark@gmail.com <godlark@gmail.com>
2012-04-11 19:22:45 +0200

    Działają ustawienia lokalne

--- Qt_Interface/main.cpp
+++ Qt_Interface/main.cpp
@@ -3,6 +3,7 @@
 #include "mainwindow.hpp"
 #include "../RegisterOfErrors.hpp"
 #include <clocale>
+#include <QtCore>
 
 extern std::string* Error::DescriptionOfErrors;
 
@@ -10,6 +11,7 @@ int main (int argc, char *argv[])
 {
    std::locale::global(std::locale("en_US"));
    setlocale(LC_ALL, "en_US");
+   QLocale::setDefault(QLocale(QLocale::English, QLocale::UnitedStates));
    Error::setDescriptionOfErrors();
    QApplication app(argc, argv);
    MainWindow window;
